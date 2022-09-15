09e0a3854b452695606b890c0a1abc6e64cfa090
Ilya Flaks <105448137+ilyaflaks@users.noreply.github.com>
2022-09-14 17:29:12 -0700

    added readme

--- index.js
+++ index.js
@@ -18,24 +18,6 @@ client.mGet(
   }
 );
 
-// function data() {
-//   return new Promise((resolve, reject) => {
-//     client.mGet(
-//       ["header", "left", "article", "right", "footer"],
-//       function (err, value) {
-//         const data = {
-//           header: Number(value[0]),
-//           left: Number(value[1]),
-//           article: Number(value[2]),
-//           right: Number(value[3]),
-//           footer: Number(value[4]),
-//         };
-//         err ? reject(null) : resolve(data);
-//       }
-//     );
-//   });
-// }
-
 function data() {
   return new Promise((resolve, reject) => {
     client.mGet(
